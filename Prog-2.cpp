3c69a89ed060f50d97cadba94b8d5a84507870b8
ytlin <eq180aaa@hotmail.com.tw>
2016-06-01 22:24:07 +0800

    v2 , fix the bug of Route()

--- Prog-2.cpp
+++ Prog-2.cpp
@@ -18,6 +18,7 @@ int VR;
 int VS;
 int des;
 int src;
+
 node** verts;
 
 /**class*/
@@ -34,6 +35,9 @@ typedef priority_queue<node, vector<node>, cmp> pq;
 
 /**functions*/
 void Dijkstra(node** v);
+//bool check(int* trace,int trace_index,int* d);
+int printSolution(int* dist, int n, int* parent);
+void printPath(int* parent, int j);
 
 int main()
 {/*
@@ -98,7 +102,12 @@ void Dijkstra(node** v)
     int trace[num_verts];
     int trace_index=0;
     bool visited[num_verts];
-    for(int i=0;i<num_verts;i++)visited[i]=false;
+    int parent[num_verts];
+    for(int i=0;i<num_verts;i++)
+    {
+        visited[i]=false;
+        parent[i]=-1;
+    }
     node* tmp;
     node n;
     pq x;
@@ -126,16 +135,64 @@ void Dijkstra(node** v)
                     d[tmp->v] = tmpD+tmp->w;
                     n.v = tmp->v; n.w=d[tmp->v]; n.next=NULL;
                     x.push(n);
+                    parent[tmp->v] = u;
+             //       cout<<"push:"<<n.v<<" "<<n.w<<endl;
                  }
                  tmp = tmp->next;
              }
         if(!visited[des])trace[trace_index++]=u;
+        /*if(trace_index>2 && check(trace,trace_index,d))
+            {
+                trace[trace_index-2] = trace[trace_index-1];
+                trace_index--;
+            }*/
         }
         x.pop();
     }
+    //printSolution(d, num_verts, parent);
     cout<<"Route: ";
-    for(int i=0;i<trace_index;i++)cout<<trace[i]<<" ";
-    cout<<des<<endl;
-    cout<<"Total distance: "<<d[des]<<endl;
+    cout<<src<<" ";
+    printPath(parent,des);
+    //for(int i=0;i<trace_index;i++)cout<<trace[i]<<" ";
+    //cout<<des<<endl;
+    cout<<"\nTotal distance: "<<d[des]<<endl;
     cout<<"Estimation time: "<<((double)d[des])/VS<<endl;
+}
+/*
+bool check(int* trace,int trace_index,int* d)
+{
+    cout<<trace_index<<"begin"<<endl;
+    node* tmp = verts[trace[trace_index-2]];
+    int tmp_d=-1;
+    while(tmp != NULL)
+    {
+        if(tmp->v == trace[trace_index-1])
+        {
+            tmp_d = tmp->w;
+            break;
+        }
+        tmp = tmp->next;
+    }
+    return (d[trace[trace_index-1]] < d[trace[trace_index-2]]+tmp_d);
+}
+*/
+void printPath(int* parent, int j)
+{
+    // Base Case : If j is source
+    if (parent[j]==-1)
+        return;
+    printPath(parent, parent[j]);
+    printf("%d ", j);
+}
+// A utility function to print the constructed distance
+// array
+
+int printSolution(int* dist, int n, int* parent)
+{
+    printf("Vertex\t  Distance\tPath");
+    for (int i = 0; i < num_verts; i++)
+    {
+        printf("\n%d -> %d \t\t %d\t\t%d ", src, i, dist[i], src);
+        printPath(parent, i);
+    }
 }
